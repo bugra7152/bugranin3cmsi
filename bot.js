c4e0bfc45ceae5d5da826cd5a0164689babe56c5
bugra7152 <31568855+bugra7152@users.noreply.github.com>
2018-11-26 21:41:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
+﻿
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -42,4 +42,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('kapalı');
+client.login(process.env.BOT_TOKEN);
