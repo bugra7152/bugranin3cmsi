45cab2945984b01d52623e97424a3c443f89dccd
bugra7152 <31568855+bugra7152@users.noreply.github.com>
2018-11-26 21:44:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ayarlar = require('./ayarlar.json');
 
 client.on('ready', () => {
 console.log(`Bot ${client.user.username} adı ile giriş yaptı!`);
