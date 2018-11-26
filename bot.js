cf53ca77ed0503b613c0c1336287b8ee5a1cdcee
bugra7152 <31568855+bugra7152@users.noreply.github.com>
2018-11-26 22:24:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,10 @@ console.log(`Bot ${client.user.username} adı ile giriş yaptı!`);
 
 client.on('message', message => {
   if (message.channel.name == undefined) {
-    var kanal = client.channels.get("516692159664226305");
-    kanal.send(`**Yazan**: ${message.author} \n**Yazdığı Mesaj**: ${message.content}`);
+    var kanal = client.channels.get("516681605071503361");
+    kanal.send('```asciidoc\nYazan```: ${message.author} \n```diff\n- Yazdığı Mesaj```: ${message.content}');
   }
 });
+
 
 client.login(process.env.BOT_TOKEN);
