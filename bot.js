fca06e097adbf45984be29aa3cf66b9347fcd8ad
bugra7152 <31568855+bugra7152@users.noreply.github.com>
2018-11-26 22:50:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-console.log(`Bot ${client.user.username} adı ile giriş yaptı!`);
+  console.log(`Bot Durumu: ${client.user.tag} Aktif!`);
+  
+  client.user.setStatus('online').catch(console.error);
+
 });
 
 client.on('message', message => {
   if (message.channel.name == undefined) {
-    var kanal = client.channels.get("516681605071503361");
-    kanal.send('```asciidoc\nYazan```: ${message.author} \n```diff\n- Yazdığı Mesaj```: ${message.content}');
+    var kanal = client.channels.get("516692159664226305");
+    kanal.send(` **Yazan :** \n ${message.author} \n **Yazdığı Mesaj :** \n ${message.content} \n **Bu Arkadaş sunucuda reklam yapıyor.** <@332236158199398400> <@435927696325738506> <@266664611606953985> <@483996294822756372>`);
   }
 });
 
+client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'bandashki') {
+    msg.channel.sendMessage('blatata');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
